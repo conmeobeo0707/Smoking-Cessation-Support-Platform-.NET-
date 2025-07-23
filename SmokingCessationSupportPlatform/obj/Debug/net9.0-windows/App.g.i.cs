15cecb900233e61b7c9f13479c07b1bcd0950c14
nhatduy111003 <phamnhatduy08@gmail.com>
2025-07-24 00:17:08 +0700

    Push comit to duy branch

--- SmokingCessationSupportPlatform/obj/Debug/net9.0-windows/App.g.i.cs
+++ SmokingCessationSupportPlatform/obj/Debug/net9.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace SmokingCessationSupportPlatform {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.7.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace SmokingCessationSupportPlatform {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.7.0")]
         public static void Main() {
             SmokingCessationSupportPlatform.App app = new SmokingCessationSupportPlatform.App();
             app.InitializeComponent();
