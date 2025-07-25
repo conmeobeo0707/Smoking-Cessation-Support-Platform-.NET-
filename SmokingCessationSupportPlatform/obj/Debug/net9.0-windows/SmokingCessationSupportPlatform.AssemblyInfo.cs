ab0fb5ae47ced4f733cdeaed9ed8ac429ac048c0
conmeobeo0707 <Conmeo07@>
2025-07-25 14:43:50 +0700

    complete coach

--- SmokingCessationSupportPlatform/obj/Debug/net9.0-windows/SmokingCessationSupportPlatform.AssemblyInfo.cs
+++ SmokingCessationSupportPlatform/obj/Debug/net9.0-windows/SmokingCessationSupportPlatform.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmokingCessationSupportPlatform")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45f5bf3572e01c1d66878d409153fa12f16ae49f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4413f8c0c8dc387645528eb81bf35efab3b045f")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmokingCessationSupportPlatform")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmokingCessationSupportPlatform")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
